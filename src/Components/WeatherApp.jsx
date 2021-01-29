9e8603a098a5bb4fa67d68125bce700e5cd0a2ef
Shwetang <shwetangsingh5@gmail.com>
2021-01-29 19:08:59 +0530

    api connected

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const WeatherApp = () => {
+    const [place, setPlace] = useState("");
+    const [search, setSearch] = useState("");
 
-    const handleInput = (event) => { };
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=8f9b5c95e274770016c9537586e353c7`;
+            const response = await fetch(url);
+
+            const resJson = await response.json();
+
+            setPlace(resJson.main);
+        };
+
+        fetchApi();
+    }, [search]);
+
+    const handleInput = (event) => { 
+        setSearch(event.target.value);
+
+    };
 
     return ( 
         <>
             <div className="box">
                 <div className="inputData">
-                    <input type="search" onChange={handleInput} className="inputField" />
+                    <input type="search" value={search} onChange={handleInput} className="inputField" />
                 </div>
 
-                <div className="info">
-                    <h2 className="location">
-                        <i className="fas fa-street-view" /> {" "}
-                        Delhi
-                    </h2>
-                    <h1 className="temperature">
-                        10°C
-                    </h1>
-                    <h3 className="temp_min-max">
-                        Min : 9°C | Max : 11°C
-                    </h3>
-                    <div className="wave -one"></div>
-                    <div className="wave -two"></div>
-                    <div className="wave -three"></div>
-                </div>
+                {!place ? (
+                    <p> Place not found !</p>
+                ) : ( 
+                    <div>
+                        <div className="info">
+                            <h2 className="location">
+                                <i className="fas fa-street-view" /> {" "}
+                                {search}
+                            </h2>
+                            <h1 className="temperature">
+                                {place.temp}°C
+                            </h1>
+                            <h3 className="temp_min-max">
+                                Min : {place.temp_min}°C | Max : {place.temp_max}°C
+                            </h3>
+                        </div>
+                
+                        <div className="wave -one" />
+                        <div className="wave -two" />
+                        <div className="wave -three" />
+                    </div>
+                )}
             </div>
         </>
     );
