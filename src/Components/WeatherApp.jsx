48af89d3b3d1d3f32e330228da5174f17f98571f
Shwetang <shwetangsingh5@gmail.com>
2021-01-29 14:03:28 +0530

    basic html temp card

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 const WeatherApp = () => {
+
+    const handleInput = (event) => { };
+
     return ( 
         <>
             <div className="box">
                 <div className="inputData">
-                    <input type="search" className="inputField"/>
+                    <input type="search" onChange={handleInput} className="inputField"/>
+                </div>
+
+                <div className="info">
+                    <h2 className="location">
+                        Dehradun
+                    </h2>
+                    <h1 className="temperature">
+                        10°C
+                    </h1>
+                    <h3 className="temp_min-max">
+                        Min : 9°C | Max : 11°C
+                    </h3>
                 </div>
             </div>
         </>
